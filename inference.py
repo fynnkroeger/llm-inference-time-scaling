f4c9010853fefd63b991872770437119627e01c7
fynn.kroeger <fynn_kroeger@web.de>
2024-11-14 18:07:01 +0100

    rename, clean up to make experiment function

--- inference.py
+++ inference.py
@@ -25,21 +25,15 @@ def run_generation(out_file, sampling_params, llm_params):
     write_jsonl(out_file, samples)
 
 
-if __name__ == "__main__":
+def run_experiment(sampling_params, llm_params, evaluate):
     environ["CUDA_VISIBLE_DEVICES"] = "3"
     environ["TOKENIZERS_PARALLELISM"] = "true"
 
     out_path = Path("/raid/shared/llm-inference-scaling/outputs")
     out_path.mkdir(exist_ok=True, parents=True)
 
-    # todo dont generate again with same settings
-
-    sampling_params = dict(temperature=0.8, top_p=0.95, n=1, max_tokens=128)
-    llm_params = dict(model="meta-llama/Llama-3.2-1B", gpu_memory_utilization=0.75)
-
     name = f"{uuid.uuid4()}.jsonl"  # choose out file name randomly
     this_params = dict(sampling_params=sampling_params, llm_params=llm_params)
-
     experiments_file = out_path / "_experiments.json"
     if experiments_file.exists():
         with open(experiments_file, "r") as f:
@@ -47,15 +41,20 @@ if __name__ == "__main__":
     else:
         experiments = {}
     if this_params in experiments.values():
-        exit()
+        return
     experiments[name] = this_params
     with open(experiments_file, "w") as f:
         json.dump(experiments, f, indent=4)
-
     out_file = out_path / name
     run_generation(out_file, sampling_params, llm_params)
+    if evaluate:
+        result = evaluation.evaluate_functional_correctness(
+            str(out_file), k=[1, 4, 16, 64, 256]
+        )
+        print(result)  # todo write results somewhere?
 
-    result = evaluation.evaluate_functional_correctness(
-        str(out_file), k=[1, 4, 16, 64, 256]
-    )
-    print(result)
+
+if __name__ == "__main__":
+    sampling_params = dict(temperature=0.8, top_p=0.95, n=4, max_tokens=128)
+    llm_params = dict(model="meta-llama/Llama-3.2-1B", gpu_memory_utilization=0.75)
+    run_experiment(sampling_params, llm_params, evaluate=True)
