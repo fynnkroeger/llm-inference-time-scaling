b7baa3419bfa9eab687a81e1e0e17f36b83df9aa
fynn.kroeger <fynn_kroeger@web.de>
2024-11-14 17:34:53 +0100

    write to random file, clean up

--- baseline.py
+++ baseline.py
@@ -3,36 +3,54 @@ from human_eval.data import write_jsonl, read_problems
 from pathlib import Path
 from time import time
 from human_eval import evaluation
+from os import environ
+import uuid
+import json
 
-def run_generation():
+
+def run_generation(out_file, sampling_params, llm_params):
     problems = read_problems()
-    samples = []
     prompts = []
     task_ids = []
-    for task_id in problems:
-        prompt = problems[task_id]["prompt"]
-        prompts.append(prompt)
+    for task_id, problem in problems.items():
+        prompts.append(problem["prompt"])
         task_ids.append(task_id)
 
-
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, n=256, max_tokens=128)
-    llm = LLM(model="meta-llama/Llama-3.2-1B", gpu_memory_utilization=0.75)
+    llm = LLM(**llm_params)
     t0 = time()
-    outputs = llm.generate(prompts, sampling_params)
+    outputs = llm.generate(prompts, SamplingParams(**sampling_params))
     print(f"generation time: {time() - t0:.3f}")
 
+    samples = []
     for tid, output in zip(task_ids, outputs):
-        # prompt = output.prompt
         for out in output.outputs:
             samples.append(dict(task_id=tid, completion=out.text))
     write_jsonl(out_file, samples)
 
+
 if __name__ == "__main__":
+    environ["CUDA_VISIBLE_DEVICES"] = "3"
+    environ["TOKENIZERS_PARALLELISM"] = "true"
+
     out_path = Path("/raid/shared/llm-inference-scaling/outputs")
     out_path.mkdir(exist_ok=True, parents=True)
-    out_file = out_path / "samples.jsonl"
-    # todo dont generate again with same settings
-    run_generation()
 
-    result = evaluation.evaluate_functional_correctness(str(out_file), k=[1, 4, 16, 64, 256], n_workers=32)
+    # todo dont generate again with same settings
+
+    sampling_params = dict(temperature=0.8, top_p=0.95, n=1, max_tokens=128)
+    llm_params = dict(model="meta-llama/Llama-3.2-1B", gpu_memory_utilization=0.75)
+
+    name = f"{uuid.uuid4()}.jsonl"  # choose out file name randomly
+    with open(out_path / "_experiments.jsonl", "a") as f:
+        info = dict(name=name, sampling_params=sampling_params, llm_params=llm_params)
+        f.write(json.dumps(info)+"\n")
+
+    out_file = out_path / name
+    run_generation(
+        out_file, sampling_params, llm_params
+    )
+
+    result = evaluation.evaluate_functional_correctness(
+        str(out_file), k=[1, 4, 16, 64, 256]
+    )
     print(result)
