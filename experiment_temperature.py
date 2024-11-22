9db7fceb6834f39bd7e71351c88d916502fce4f0
Tonybodo <arnold4@uni-potsdam.de>
2024-11-22 15:11:25 +0100

    Refactor: Experiment setup

--- experiment_temperature.py
+++ experiment_temperature.py
@@ -1,6 +1,17 @@
 from inference import run_experiment
+import yaml
 
-for temperature in [0.6, 0.8, 1, 1.2]:
-    sampling_params = dict(temperature=temperature, n=256, max_tokens=128)
-    llm_params = dict(model="meta-llama/Llama-3.1-8B", gpu_memory_utilization=0.75)
-    run_experiment(sampling_params, llm_params, evaluate=False)
+
+try:
+    config_path = "./config.yaml"
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    for temperature in [0.2, 0.4, 0.6, 0.8, 1, 1.2]:
+        # Set temperature for each run
+        config["sampling"]["temperature"] = temperature
+        out_path = run_experiment(config)
+        print(f"Results successfully stored in {out_path}")
+except FileNotFoundError:
+    print(f"Config file not found: {config_path}")
+except yaml.YAMLError as e:
+    print(f"Error in YAML file: {e}")
