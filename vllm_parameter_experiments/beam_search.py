24d88d5f5a6257065cc92010d562b6dcfa62288b
fynn.kroeger <fynn_kroeger@web.de>
2025-01-16 12:39:20 +0100

    add eval

--- vllm_parameter_experiments/beam_search.py
+++ vllm_parameter_experiments/beam_search.py
@@ -4,8 +4,8 @@ from vllm_parameter_experiments.run_eval import evaluate_and_save_results, calc_
 output_files = {}
 
 models = ["meta-llama/Llama-3.2-1B"]
-for temperature in [0.6, 0.8, 1]:
-    for width in [2, 8, 32]:
+for temperature in [0.6]:
+    for width in [4]:
         for model in models:
             sampling_params = dict(temperature=temperature, beam_width=width, max_tokens=128)
             llm_params = dict(model=model, gpu_memory_utilization=0.75)
@@ -17,4 +17,6 @@ result_files = []
 for out_file in output_files:
     result_files.append(evaluate_and_save_results(out_file))
 
-# todo compare with other
+for (out_file, config), result_file in zip(output_files.items(), result_files):
+    pass_at_k = calc_pass_at_k_from_results(result_file, [4])
+    print(pass_at_k)
