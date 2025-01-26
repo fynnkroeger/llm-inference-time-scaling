df4cd25f5c591a269376b394dcca036166a2ac70
fynn.kroeger <fynn_kroeger@web.de>
2025-01-26 17:06:12 +0100

    refactor vllm beam search

--- vllm_parameter_experiments/beam_search.py
+++ vllm_parameter_experiments/beam_search.py
@@ -1,15 +1,42 @@
+import os
+
+from vllm import LLM
+from vllm.sampling_params import BeamSearchParams
+from human_eval.data import write_jsonl, read_problems
+from time import time
+
 from vllm_parameter_experiments.inference import run_experiment, plots_path
 from vllm_parameter_experiments.run_eval import evaluate_and_save_results, calc_pass_at_k_from_results
 
 output_files = {}
 
+
+def run_generation_vllm_beam(out_file, sampling_params, llm_params):
+    problems = read_problems()
+    prompts = [problem["prompt"] for problem in problems.values()]
+    task_ids = list(problems.keys())
+
+    llm = LLM(**llm_params)
+    t0 = time()
+    outputs = llm.beam_search(prompts, BeamSearchParams(**sampling_params))
+    generation_time = time() - t0
+    samples = []
+    for tid, output in zip(task_ids, outputs):
+        for out in output.sequences:
+            samples.append(dict(task_id=tid, completion=out.text))
+    write_jsonl(out_file, samples)
+    return generation_time
+
+
 models = ["meta-llama/Llama-3.2-1B"]
 for temperature in [0.6]:
-    for width in [4]:
+    for width in [2, 4]:
         for model in models:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "7"
             sampling_params = dict(temperature=temperature, beam_width=width, max_tokens=128)
             llm_params = dict(model=model, gpu_memory_utilization=0.75)
-            out_file = run_experiment(sampling_params, llm_params, beam_search=True)
+            out_file = run_experiment(sampling_params, llm_params, generation_function=run_generation_vllm_beam,
+                                      force_generation=True)
             output_files[out_file] = dict(temperature=temperature, model=model, beam_width=width)
             print("done", temperature, width)
 
--- vllm_parameter_experiments/inference.py
+++ vllm_parameter_experiments/inference.py
@@ -1,9 +1,7 @@
 from vllm import LLM, SamplingParams
-from vllm.sampling_params import BeamSearchParams
 from human_eval.data import write_jsonl, read_problems
 from pathlib import Path
 from time import time
-from human_eval import evaluation
 from os import environ
 import uuid
 import json
@@ -16,33 +14,24 @@ plots_path = experiment_path / "plots"
 plots_path.mkdir(exist_ok=True, parents=True)
 
 
-# todo beam search as separate function
-def run_generation_vllm(out_file, sampling_params, llm_params, beam_search=False):
+def run_generation_vllm(out_file, sampling_params, llm_params):
     problems = read_problems()
     prompts = [problem["prompt"] for problem in problems.values()]
     task_ids = list(problems.keys())
 
     llm = LLM(**llm_params)
     t0 = time()
+    outputs = llm.generate(prompts, SamplingParams(**sampling_params))
+    generation_time = time() - t0
     samples = []
-    if beam_search:
-        outputs = llm.beam_search(prompts, BeamSearchParams(**sampling_params))
-        generation_time = time() - t0
-        for tid, output in zip(task_ids, outputs):
-            for out in output.sequences:
-                samples.append(dict(task_id=tid, completion=out.text))
-
-    else:
-        outputs = llm.generate(prompts, SamplingParams(**sampling_params))
-        generation_time = time() - t0
-        for tid, output in zip(task_ids, outputs):
-            for out in output.outputs:
-                samples.append(dict(task_id=tid, completion=out.text))
+    for tid, output in zip(task_ids, outputs):
+        for out in output.outputs:
+            samples.append(dict(task_id=tid, completion=out.text))
     write_jsonl(out_file, samples)
     return generation_time
 
 
-def run_experiment(sampling_params, llm_params, beam_search=False, force_generation=False,
+def run_experiment(sampling_params, llm_params, force_generation=False,
                    generation_function=run_generation_vllm):
     environ["TOKENIZERS_PARALLELISM"] = "true"
     environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
@@ -69,11 +58,11 @@ def run_experiment(sampling_params, llm_params, beam_search=False, force_generat
             ):
                 print("experiment already performed, skipping")
                 return file_name
-    print("running experiment", config)
+    print("running experiment", sampling_params, llm_params)
     name = f"{uuid.uuid4()}.jsonl"  # choose out file name randomly
     out_file = output_path / name
     num_gpus_used = len(environ["CUDA_VISIBLE_DEVICES"].split(","))
-    raw_time = generation_function(out_file, sampling_params, llm_params, beam_search=beam_search)
+    raw_time = generation_function(out_file, sampling_params, llm_params)
 
     # write only when completed
     experiments[name] = dict(
@@ -90,4 +79,4 @@ def run_experiment(sampling_params, llm_params, beam_search=False, force_generat
 if __name__ == "__main__":
     sampling_params = dict(temperature=0.6, n=256, max_tokens=128)
     llm_params = dict(model="meta-llama/Llama-3.1-8B", gpu_memory_utilization=0.75)
-    run_experiment(sampling_params, llm_params, evaluate=False)
+    run_experiment(sampling_params, llm_params)
