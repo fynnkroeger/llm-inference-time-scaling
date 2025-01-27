0e0e0967b8aff825b8dd153578315264482b7c22
fynn.kroeger <fynn_kroeger@web.de>
2025-01-27 12:03:33 +0100

    separate plotting for 3B

--- vllm_parameter_experiments/beam_search_hf.py
+++ vllm_parameter_experiments/beam_search_hf.py
@@ -8,6 +8,7 @@ import torch
 from time import sleep
 from vllm_parameter_experiments.run_eval import evaluate_and_save_results, calc_pass_at_k_from_results
 from vllm_parameter_experiments.inference import run_experiment, plots_path
+import matplotlib.pyplot as plt
 
 experiment_path = Path("/raid/shared/llm-inference-scaling/vllm_parameter_experiments")
 output_path = experiment_path / "outputs"
@@ -51,41 +52,40 @@ def run_hf(out_file, sampling_params, llm_params):
     return generation_time
 
 
-if __name__ == "__main__":
-    output_files = {}
-    configs = []
+output_files = {}
+configs = []
 
-    for width in [6, 4, 2]:  # 16 does not work
-        print(environ["CUDA_VISIBLE_DEVICES"])
-        for early_stopping in [True, False]:
-            for repetition_penalty in [1.0, 1.1, 1.2]:
-                # normal beam search
+for width in [6, 4, 2]:  # 16 does not work
+    print(environ["CUDA_VISIBLE_DEVICES"])
+    for early_stopping in [True, False]:
+        for repetition_penalty in [1.0, 1.1, 1.2]:
+            # normal beam search
+            configs.append(dict(max_new_tokens=128,
+                                num_beams=width, num_return_sequences=width,
+                                repetition_penalty=repetition_penalty,
+                                do_sample=False, early_stopping=early_stopping))
+
+            # sampling beam search
+            for temperature in [0.6, 1.0]:
                 configs.append(dict(max_new_tokens=128,
                                     num_beams=width, num_return_sequences=width,
-                                    repetition_penalty=repetition_penalty,
-                                    do_sample=False, early_stopping=early_stopping))
+                                    repetition_penalty=repetition_penalty, temperature=temperature,
+                                    do_sample=True, early_stopping=early_stopping))
 
-                # sampling beam search
-                for temperature in [0.6, 1.0]:
+            # diverse beam search
+            for num_beam_groups in [2, width]:
+                for diversity_penalty in [1.0]:
                     configs.append(dict(max_new_tokens=128,
                                         num_beams=width, num_return_sequences=width,
-                                        repetition_penalty=repetition_penalty, temperature=temperature,
-                                        do_sample=True, early_stopping=early_stopping))
-
-                # diverse beam search
-                for num_beam_groups in [2, width]:
-                    for diversity_penalty in [1.0]:
-                        configs.append(dict(max_new_tokens=128,
-                                            num_beams=width, num_return_sequences=width,
-                                            repetition_penalty=repetition_penalty,
-                                            num_beam_groups=num_beam_groups,
-                                            diversity_penalty=diversity_penalty, do_sample=False,
-                                            early_stopping=early_stopping))
+                                        repetition_penalty=repetition_penalty,
+                                        num_beam_groups=num_beam_groups,
+                                        diversity_penalty=diversity_penalty, do_sample=False,
+                                        early_stopping=early_stopping))
 
 # todo preven parameter offloading
 # "Some parameters are on the meta device because they were offloaded to the cpu."
 
-for n in [1, 2, 4, 6, 8, 16]:
+for n in [1, 2, 4, 6]:
     configs.append(dict(max_new_tokens=128, do_sample=True, temperature=0.7, num_return_sequences=n))
 
 devices = "4,5,6,7".split(",")
@@ -96,7 +96,7 @@ for model in models:
     for n in [1, 2, 4, 8, 16, 32, 64]:
         out_file = run_experiment(sampling_params=dict(temperature=0.7, n=n, max_tokens=128),
                                   llm_params=dict(model=model, gpu_memory_utilization=0.75))
-        output_files[out_file] = dict(temperature=temperature, n=n)
+        output_files[out_file] = dict(temperature=temperature, n=n, model_name=model)
 
     for sampling_params in configs:
         n = 1
@@ -109,7 +109,8 @@ for model in models:
                 break
             except RuntimeError:
                 n *= 2
-        output_files[out_file] = sampling_params
+        output_files[out_file] = dict(**sampling_params, model_name=model)
+
         print()
 for k, v in output_files.items():
     print(k, v)
@@ -122,60 +123,63 @@ sleep(1)
 with open(experiments_file, "r") as f:
     experiments = json.load(f)
 
-import matplotlib.pyplot as plt
+for model in models:
+    # Data collection for scatter plot
+    times = []  # To store time_taken
+    pass_ks = []  # To store pass@k values
+    ks = []
 
-# Data collection for scatter plot
-times = []  # To store time_taken
-pass_ks = []  # To store pass@k values
-ks = []
+    times_rep = []
+    pass_ks_rep = []
+    ks_rep = []
 
-times_rep = []
-pass_ks_rep = []
-ks_rep = []
+    vllm_pass = []
+    vllm_times = []
+    vllm_ks = []
 
-vllm_pass = []
-vllm_times = []
-vllm_ks = []
+    for (out_file, config), result_file in zip(output_files.items(), result_files):
+        if config["model_name"] != model:
+            continue
+        if "n" in config:
+            pass_at_k = calc_pass_at_k_from_results(result_file, [config["n"]])
+            time_taken = experiments[str(out_file)]["generation_time"]
+            pass_k_value = list(pass_at_k.values())[0]
+            vllm_ks.append(config["n"])
+            vllm_times.append(time_taken)
+            vllm_pass.append(pass_k_value)
+        elif "num_beams" in config:
+            pass_at_k = calc_pass_at_k_from_results(result_file, [config["num_beams"]])
+            time_taken = experiments[str(out_file)]["generation_time"]
+            pass_k_value = list(pass_at_k.values())[0]
+            ks.append(config["num_beams"])
+            times.append(time_taken)
+            pass_ks.append(pass_k_value)
+            print(f"pass@k {pass_k_value: .2f} ;", f"{round(time_taken)} H100-sec", config)
+        else:
+            pass_at_k = calc_pass_at_k_from_results(result_file, [config["num_return_sequences"]])
+            time_taken = experiments[str(out_file)]["generation_time"]
+            pass_k_value = list(pass_at_k.values())[0]
+            ks_rep.append(config["num_return_sequences"])
+            times_rep.append(time_taken)
+            pass_ks_rep.append(pass_k_value)
 
-for (out_file, config), result_file in zip(output_files.items(), result_files):
-    if "n" in config:
-        pass_at_k = calc_pass_at_k_from_results(result_file, [config["n"]])
-        time_taken = experiments[str(out_file)]["generation_time"]
-        pass_k_value = list(pass_at_k.values())[0]
-        vllm_ks.append(config["n"])
-        vllm_times.append(time_taken)
-        vllm_pass.append(pass_k_value)
-    elif "num_beams" in config:
-        pass_at_k = calc_pass_at_k_from_results(result_file, [config["num_beams"]])
-        time_taken = experiments[str(out_file)]["generation_time"]
-        pass_k_value = list(pass_at_k.values())[0]
-        ks.append(config["num_beams"])
-        times.append(time_taken)
-        pass_ks.append(pass_k_value)
-        print(f"pass@k {pass_k_value: .2f} ;", f"{round(time_taken)} H100-sec", config)
-    else:
-        pass_at_k = calc_pass_at_k_from_results(result_file, [config["num_return_sequences"]])
-        time_taken = experiments[str(out_file)]["generation_time"]
-        pass_k_value = list(pass_at_k.values())[0]
-        ks_rep.append(config["num_return_sequences"])
-        times_rep.append(time_taken)
-        pass_ks_rep.append(pass_k_value)
+    model_name = model.split("/")[-1]
+    # Scatter plot
+    plt.figure()
+    plt.scatter(times, pass_ks, marker="+", label="HF beam search")
+    plt.xlabel("Time Taken (H100-sec)")
+    plt.ylabel("pass@k")
+    plt.title("Scatter Plot of pass@k vs Time Taken")
+    plt.plot(vllm_times, vllm_pass, label="vLLM repeated sampling")
+    plt.plot(times_rep, pass_ks_rep, label="HF repeated sampling")
+    plt.legend()
+    plt.xscale("log")
+    plt.savefig(f"out_{model_name}.png")  # print time and pass at k so we can look at the plot and compare performance
 
-# Scatter plot
-plt.scatter(times, pass_ks, marker="+", label="HF beam search")
-plt.xlabel("Time Taken (H100-sec)")
-plt.ylabel("pass@k")
-plt.title("Scatter Plot of pass@k vs Time Taken")
-plt.plot(vllm_times, vllm_pass, label="vLLM repeated sampling")
-plt.plot(times_rep, pass_ks_rep, label="HF repeated sampling")
-plt.legend()
-plt.xscale("log")
-plt.savefig("out.png")  # print time and pass at k so we can look at the plot and compare performance
-
-plt.figure()
-plt.plot(vllm_ks, vllm_pass, label="vLLM repeated sampling")
-plt.plot(ks_rep, pass_ks_rep, label="HF repeated sampling")
-plt.scatter(ks, pass_ks, marker="+", label="HF beam search")
-plt.legend()
-plt.xscale("log")
-plt.savefig("out2.png")
+    plt.figure()
+    plt.plot(vllm_ks, vllm_pass, label="vLLM repeated sampling")
+    plt.plot(ks_rep, pass_ks_rep, label="HF repeated sampling")
+    plt.scatter(ks, pass_ks, marker="+", label="HF beam search")
+    plt.legend()
+    plt.xscale("log")
+    plt.savefig(f"out2_{model_name}.png")
